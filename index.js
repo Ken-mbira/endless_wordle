ef5955dad3565dbf522b2fd17dae1ca772742113
ken <ken.mbira@student.moringaschool.com>
2022-05-10 10:06:21 +0300

    add keyboard keys

---
+++ index.js
@@ -0,0 +1,45 @@
+const tileContainer = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', () => handleClick(key))
+    keyboard.appendChild(buttonElement)
+})
+
+const handleClick = (key) => {
+    console.log(`clicked ${key}`)
+}
