23471aebbe8780a26c829be492f13f9bfeb8b880
ken <ken.mbira@student.moringaschool.com>
2022-05-10 10:36:08 +0300

    add display tile grid

--- index.js
+++ index.js
@@ -32,6 +32,35 @@ const keys = [
     '«',
 ]
 
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', `guessRow-${guessRowIndex}`)
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', `guessRow-${guessRow}-tile${guessIndex}`)
+        tileElement.classList.add("tile")
+        rowElement.append(tileElement)
+    })
+    tileContainer.append(rowElement)
+})
+
+document.addEventListener("keydown", event => {
+    if (event.key.toUpperCase() === "BACKSPACE") {
+        handleClick(keys[keys.length - 1])
+    } else if (keys.includes(event.key.toUpperCase())) {
+        handleClick(event.key.toUpperCase())
+    }
+});
+
 keys.forEach(key => {
     const buttonElement = document.createElement('button')
     buttonElement.textContent = key
@@ -41,5 +70,5 @@ keys.forEach(key => {
 })
 
 const handleClick = (key) => {
-    console.log(`clicked ${key}`)
+    console.log(key)
 }
